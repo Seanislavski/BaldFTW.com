4c42062547b88adbba04bcea12ffe9158b3d5819
Sean Rivis <sean.rivis@gmail.com>
2017-05-31 12:56:49 -0400

    Added bg images to section, box class and adjusted nav

--- script/main.js
+++ script/main.js
@@ -1,1 +1,3 @@
-// $(".subBox1").html("hey you");
+// $(".box1").html("hey you");
+// $("#sect3").on('click'{$(this).delay(1000);})
+$("a[href^=#]").click(function(e) { e.preventDefault(); var dest = $(this).attr('href'); console.log(dest); $('html,body').animate({ scrollTop: $(dest).offset().top }, 'slow'); });
