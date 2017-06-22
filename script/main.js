b8ca004e6429fea8a851da7948b31519093b5353
Sean <Sean.Rivis@gmail.com>
2017-06-22 18:40:05 -0400

    javascript edit

--- script/main.js
+++ script/main.js
@@ -1,3 +1,4 @@
 // $(".box1").html("hey you");
 // $("#sect3").on('click'{$(this).delay(1000);})
 $("a[href^=#]").click(function(e) { e.preventDefault(); var dest = $(this).attr('href'); console.log(dest); $('html,body').animate({ scrollTop: $(dest).offset().top }, 'slow'); });
+$(".footer").click(function(){alert "Hi"});
