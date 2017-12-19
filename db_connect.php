c13df137b1070c1a5dbfa02246ff3bcb4f4eeae2
Sean Rivis <sean.rivis@gmail.com>
2017-12-19 17:00:32 -0500

    Connect to MYSQL and dynamically call for posts

---
+++ db_connect.php
@@ -0,0 +1,35 @@
+<?php
+    $dbhost = "localhost";
+    $dbuser = "Sean";
+    $dbpass = "createF3rr3t";
+    $dbname = "baldb";
+    $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+    //Test if connection occurred
+    if(mysqli_connect_errno()) {
+        die("Database connection failed: " .
+            myqli_connect_error() .
+            " (" . mysqli_connect_errno() . ")"
+        );
+    }
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+            $query = "SELECT * FROM users";
+            $result = mysqli_query($conn, $query);
+            //test if there was a query error
+            if($conn == false){
+                //try to reconnect
+            } else {
+                $test = "Connected Successfully";
+            }
+            if(!$result) {
+                die("Database query failed.");
+            } else {
+                $test .= "Connected Successfully";
+            }
+            if($conn->connect_error){
+                die("Connection failed: " . $conn->connect_error);
+            } else {
+                $test .= "Connected Successfully";
+            }
+?>
---
+++ index.php
@@ -0,0 +1,82 @@
+<?php
+require_once('./db_connect.php');
+require_once('./functions.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Bald (for the) win . com</title>
+    <link rel="stylesheet" href="style/style.css">
+
+</head>
+<body>
+    <span class="mainBody"> <!-- Div 1-->
+    <a class="anchor" id="top"></a>
+        <div class="nav">
+            <h1 id="title">Sean Baldwin</h1>
+            <h3 id="slogan">Playing for Productivity</h3>
+            <ul id="navBar">
+                <li class="navItem"><a href="#top">Home</a></li>
+                <li class="navItem"><a href="#middle">Contact</a></li>
+                <li class="navItem"><a href="#bottom">About</a></li>
+            </ul>
+        </div>
+        <section class="box box1" id="box1">
+
+            <div class="inBox l1">
+                <ul class="noStyle">
+                    <li>
+                        <?php get_header(1); ?>
+                    </li>
+                    <li>
+                        <?php get_header(2); ?>
+                    </li>
+                </ul>
+            </div>
+            <div class="inBox c1">
+                <ul class="noStyle">
+                    <li>
+                        <?php
+                        get_post(1);
+                        ?>
+                    </li>
+                    <li>
+                        <?php
+                        get_post(2);
+                        ?>
+                    </li>
+                </ul>
+                <a class="lgsymb" href="#middle">
+                    <svg version="1.2" baseProfile="tiny" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" id="darrow"><path d="M16.707 13.293c-.391-.391-1.023-.391-1.414 0l-2.293 2.293v-7.586c0-.552-.447-1-1-1s-1 .448-1 1v7.586l-2.293-2.293c-.391-.391-1.023-.391-1.414 0s-.391 1.023 0 1.414l4.707 4.707 4.707-4.707c.391-.391.391-1.023 0-1.414z"/></svg>
+                </a>
+
+            </div>
+            <div class="inBox r1">
+                <span id="image">
+                    <!-- <img id="me" src="images/me.jpg"> -->
+                </span>
+                Sean Baldwin, 2015
+            </div>
+
+        </section>
+        <section class="box box2" id="box2">
+            <a class="anchor" id="middle"></a>
+            <h2 class="inBox termite">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam ea facere quidem tempore libero quaerat illo voluptas iusto consequatur perferendis. Animi iusto, repellat corporis voluptatum dolores nihil, obcaecati ducimus. Earum.
+            </h2>
+        </section>
+        <section class="box box3" id="box3">
+            <a class="anchor" id="bottom"></a>
+            <h3 class="inBox termite">
+                Sean Baldwin is a web developer and systems administrator based in New York City. He has over 5 years of experience working for managed service providers providing his customers with world class customer service, computer repair, network maintenance / administration and more.
+            </h3>
+        </section>
+        <section class="footer">
+            &copy; 2017 Sean Baldwin
+        </section>
+    </div>
+    <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+    <script type=text/javascript src="script/main.js"></script>
+</body>
+</html>
